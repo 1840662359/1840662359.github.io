5b91f69f71475d50f681aa17019b8380fcda8118
MoY丶沫影 <1840662359@qq.com>
2024-06-20 23:36:03 +0800

    更新下载地址

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -1,7 +1,7 @@
 export const redirects = JSON.parse("{}")
 
 export const routes = Object.fromEntries([
-  ["/help.html", { loader: () => import(/* webpackChunkName: "help.html" */"D:/web_site/docs/.vuepress/.temp/pages/help.html.js"), meta: {"title":"使用帮助"} }],
+  ["/help.html", { loader: () => import(/* webpackChunkName: "help.html" */"D:/web_site/docs/.vuepress/.temp/pages/help.html.js"), meta: {"title":"使用教程"} }],
   ["/", { loader: () => import(/* webpackChunkName: "index.html" */"D:/web_site/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":"首页"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"D:/web_site/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
--- docs/index.md
+++ docs/index.md
@@ -3,12 +3,12 @@ home: true
 title: 首页
 heroImage: /images/logo.webp
 actions:
-  - text: 立即下载
-    link: https://www.123pan.com/s/epe3jv-hEIOd.html
-    type: primary
-    
   - text: 使用教程
     link: /help.md
+    type: primary
+
+  - text: 立即下载
+    link: https://gitee.com/qianlong323/qianlong323/releases/download/v33/app-release_v33.apk
     type: second
 
 features:
