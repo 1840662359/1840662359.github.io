38cfbc3031a469dedadb4f869afb21ccb9dfeeb5
MoY丶沫影 <1840662359@qq.com>
2024-09-21 12:33:46 +0800

    更正教程

--- docs/.vuepress/.temp/pages/help.html.vue
+++ docs/.vuepress/.temp/pages/help.html.vue
@@ -1,5 +1,5 @@
 <template><div><h2 id="授权教程" tabindex="-1"><a class="header-anchor" href="#授权教程"><span>授权教程：</span></a></h2>
-<p>优化器支持两种授权方式，<strong>只需要选择一种方式完成授权</strong>即可使用。建议优先选择【<em>文件管理器</em>】方式授权，如果不能授权再尝试【<em>Shizuku</em>】方式授权</p>
+<p>优化器支持两种授权方式，<strong>只需要选择一种方式完成授权</strong>即可使用。建议优先选择【<strong>文件管理器</strong>】方式授权，如果不能授权再尝试【<strong>Shizuku</strong>】方式授权</p>
 <p><RouteLink to="/help-document.html">点此查看如何使用【<strong>文件管理器</strong>】进行授权</RouteLink></p>
 <p><RouteLink to="/help-shizuku.html">点此查看如何使用【<strong>Shizuku</strong>】进行授权</RouteLink></p>
 <h2 id="修改教程" tabindex="-1"><a class="header-anchor" href="#修改教程"><span>修改教程：</span></a></h2>
