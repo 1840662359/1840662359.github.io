05ccb41802b4765476398b4b441ba6ad2cc39242
MoY丶沫影 <1840662359@qq.com>
2024-10-15 16:31:25 +0800

    更新下载页

--- docs/.vuepress/.temp/pages/download-center.html.vue
+++ docs/.vuepress/.temp/pages/download-center.html.vue
@@ -1,14 +1,12 @@
 <template><div><h2 id="版本信息" tabindex="-1"><a class="header-anchor" href="#版本信息"><span>版本信息</span></a></h2>
 <ul>
-<li>最新版本：v50</li>
-<li>发布时间：2024-10-12</li>
+<li>最新版本：v51</li>
+<li>发布时间：2024-10-15</li>
 </ul>
-<p><a href="https://wwwj.lanzouw.com/inVjU2cbzykf" target="_blank" rel="noopener noreferrer">&gt;&gt;下载地址</a></p>
+<p><a href="https://wwwj.lanzouw.com/isgEW2clutwh" target="_blank" rel="noopener noreferrer">&gt;&gt;下载地址</a></p>
 <h2 id="更新日志" tabindex="-1"><a class="header-anchor" href="#更新日志"><span>更新日志：</span></a></h2>
 <ul>
-<li>修复部分设备上修改后键位偏移的问题</li>
-<li>移除包内无用资源</li>
-<li>优化读写逻辑</li>
+<li>修复无法连接服务器的问题</li>
 </ul>
 </div></template>
 
--- docs/download-center.md
+++ docs/download-center.md
@@ -1,5 +1,13 @@
 ---
 title: 下载
+head:
+  - - meta
+    - http-equiv: Cache-Control
+      content: no-cache, no-store, must-revalidate
+    - http-equiv: Pragma
+      content: no-cache
+    - http-equiv: Expires
+      content: 0
 ---
 
 ## 版本信息
