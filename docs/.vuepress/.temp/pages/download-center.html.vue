f9e49215213b93b0e14108b1d7585b50ba5470a4
MoY丶沫影 <1840662359@qq.com>
2024-10-16 15:01:20 +0800

    v52版本

--- docs/.vuepress/.temp/pages/download-center.html.vue
+++ docs/.vuepress/.temp/pages/download-center.html.vue
@@ -1,12 +1,14 @@
 <template><div><h2 id="版本信息" tabindex="-1"><a class="header-anchor" href="#版本信息"><span>版本信息</span></a></h2>
 <ul>
-<li>最新版本：v51</li>
-<li>发布时间：2024-10-15</li>
+<li>最新版本：v52</li>
+<li>发布时间：2024-10-16</li>
 </ul>
-<p><a href="https://wwwj.lanzouw.com/isgEW2clutwh" target="_blank" rel="noopener noreferrer">&gt;&gt;下载地址</a></p>
+<p><a href="https://wwwj.lanzouw.com/iaxie2coheoj" target="_blank" rel="noopener noreferrer">&gt;&gt;下载地址</a></p>
 <h2 id="更新日志" tabindex="-1"><a class="header-anchor" href="#更新日志"><span>更新日志：</span></a></h2>
 <ul>
-<li>修复无法连接服务器的问题</li>
+<li>修复UC模式下分辨率修改不生效的问题</li>
+<li>修复异形屏边距修改时偶现不正常的问题</li>
+<li>优化布局显示效果</li>
 </ul>
 </div></template>
 
